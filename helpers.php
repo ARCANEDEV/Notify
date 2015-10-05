d8035debb241617032211193f5b4515513b208c0
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-05 22:38:33 +0100

    Updating the helper

--- helpers.php
+++ helpers.php
@@ -4,16 +4,17 @@ if ( ! function_exists('notify')) {
     /**
      * Notify Helper function
      *
-     * @param  string|null $message
+     * @param  string|null  $message
      *
-     * @return \Arcanedev\Notify\Notify
+     * @return \Arcanedev\Notify\Contracts\NotifyInterface
      */
     function notify($message = null)
     {
+        /** @var Arcanedev\Notify\Contracts\NotifyInterface $notifier */
         $notifier = app('arcanedev.notify');
 
         if ( ! is_null($message)) {
-            return $notifier->info($message);
+            $notifier->info($message);
         }
 
         return $notifier;
