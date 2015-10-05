22f059180cfc434cd0c887d9e7526846baca1342
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-05 22:35:31 +0100

    Adding NotifyInterface

---
+++ src/Contracts/NotifyInterface.php
@@ -0,0 +1,77 @@
+<?php namespace Arcanedev\Notify\Contracts;
+
+/**
+ * Interface  NotifyInterface
+ *
+ * @package   Arcanedev\Notify\Contracts
+ * @author    ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+interface NotifyInterface
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Flash an information message.
+     *
+     * @param  string  $message
+     *
+     * @return self
+     */
+    public function info($message);
+
+    /**
+     * Flash a success message.
+     *
+     * @param  string  $message
+     *
+     * @return self
+     */
+    public function success($message);
+
+    /**
+     * Flash an error message.
+     *
+     * @param  string  $message
+     *
+     * @return self
+     */
+    public function error($message);
+
+    /**
+     * Flash a warning message.
+     *
+     * @param  string  $message
+     *
+     * @return self
+     */
+    public function warning($message);
+
+    /**
+     * Flash a general message.
+     *
+     * @param  string  $message
+     * @param  string  $level
+     *
+     * @return self
+     */
+    public function message($message, $level = 'info');
+
+    /**
+     * Flash an overlay modal.
+     *
+     * @param  string  $message
+     * @param  string  $title
+     *
+     * @return self
+     */
+    public function overlay($message, $title = 'Notice');
+
+    /**
+     * Add an "important" flash to the session.
+     *
+     * @return self
+     */
+    public function important();
+}
